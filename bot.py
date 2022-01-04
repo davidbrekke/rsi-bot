cacccc43b9d68498360892820a343f92bb800f35
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-01-03 20:07:31 -0600

    add rsi logic

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = "ETHUSD"
+TRADE_QUANTITY = 0.001
+
 closes = []
+in_position = False
 
 def on_open(ws):
     print("--- opened connection ---")
@@ -14,7 +21,7 @@ def on_close(ws, close_status_code, close_msg):
     print("--- closed connection ---")
 
 def on_message(ws, message):
-    global closes
+    global closes, in_position
     print("- received message -")
     json_message = json.loads(message)
     # pprint.pprint(json_message)
@@ -33,6 +40,40 @@ def on_message(ws, message):
         print(f"$ CLOSE at: ${float(close)}")
         closes.append(float(close))
         print(f"closes: {closes}")
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print(f"rsi:\n{rsi}")
+
+            last_rsi = rsi[-1]
+            print(f"latest rsi: {last_rsi}")
+
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print(f"rsi: {last_rsi} > {RSI_OVERBOUGHT}")
+                    print("--- overbought ---")
+                    print("--- SELL ! ---")
+                    # @TODO: add binance logic to sell
+                else:
+                    print(f"rsi: {last_rsi} > {RSI_OVERBOUGHT}")
+                    print("--- overbought ---")
+                    print("--- no position ---")
+
+            if last_rsi > RSI_OVERSOLD:
+                if in_position:
+                    print(f"rsi: {last_rsi} > {RSI_OVERSOLD}")
+                    print("--- oversold ---")
+                    print("--- IN POSITION ! ---")
+                else:
+                    print(f"rsi: {last_rsi} > {RSI_OVERSOLD}")
+                    print("--- oversold ---")
+                    print("--- BUY ! ---")
+                    # @TODO: add binance logic to buy
+
+
+
         print("-" * 20)
 
 def on_error(ws, error):
